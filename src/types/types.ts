592d8d61a939fbff2a95b0d5397f48cb039f2198
Dziugas Juozapaitis <dziugasj@gmail.com>
2023-02-05 16:45:10 +0200

    Types configured

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,4 +1,24 @@
+export type ProductType = "SMOOTHIE" | "ICE_CREAM";
+
+export type NutrientType = "MACRO" | "MICRO";
+
+export type MacroNutrient = "FAT" | "CARB" | "FIBRE" | "PROTEIN";
+
+export type MicroNutrient = "VIT_D" | "VIT_A" | "CALCIUM" | "POTASSIUM";
+
+export type NutritionalValue = {
+  id: number;
+  nutrientType: NutrientType;
+  macroNutrient?: MacroNutrient;
+  microNutrient?: MicroNutrient;
+  nutrientUnit: string;
+  nutrientValue: number;
+};
+
 export type Product = {
   id: number;
+  productType: ProductType;
   name: string;
+  basicDetails: string;
+  nutritionalValues: NutritionalValue[];
 };
